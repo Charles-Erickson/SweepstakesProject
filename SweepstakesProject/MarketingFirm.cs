1685fd4a11773758036e6dc3e1fa9d4395dcb1ec
Charles Erickson <cse621@gmail.com>
2019-04-17 17:00:01 -0500

    added switch case to marketing class for factory design

--- SweepstakesProject/MarketingFirm.cs
+++ SweepstakesProject/MarketingFirm.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Sweepstakes;
 
 namespace SweepstakesProject
 {
     class MarketingFirm 
     {
-     
+        public SweepstakeStackManager sweepstakeStackManager = new SweepstakeStackManager();
+
 
         public void ChooseStackOrQueue()
         {
-            Console.WriteLine("");
+            Console.WriteLine("Would you Like to use Queue or Stack data structure?");
+            string data = Console.ReadLine();
+            switch (data)
+            {
+                case "Stack":
+                    data = "Stack";
+                    return data;
+
+                case "Queue":
+                    data = "Queue";
+                    return data;
+
+
+                 
+        
+
+
+            }
         }
         
     }
