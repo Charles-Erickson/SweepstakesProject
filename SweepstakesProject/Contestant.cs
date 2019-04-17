1c0fcae736aac2b96d92fb4fda0d9f34f9779a9a
Charles Erickson <cse621@gmail.com>
2019-04-17 08:47:50 -0500

    first commit with classes for constestant sweepskates, marketingfirm, etc

---
+++ SweepstakesProject/Contestant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sweepstakes
+{
+    class Contestent
+    {
+        string FirstName;
+        string LastName;
+        string Email;
+        int RegistrationNumber;
+    }
+}
